401fb1dcd9e62dacaeb6d339763ca6a8d8ad31aa
Ibrahim Kedir <techofreact@gmail.com>
2025-05-12 14:16:12 +0300

    ft: prepare routes.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,9 @@
+# Note: these are just routes like your express app.
 from django.urls import path
-# for media
 
 from . import views
 
 urlpatterns = [
-    path("notes/", views.NoteListCreate.as_view(), name="note-list-create"),
-    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="note-delete"),
     path("children/", views.ChildListCreate.as_view(), name="children-list-create" ),
     path("user/profile/<str:firebase_uid>/", views.UserDetailsView.as_view(), name="user-details"),
     path("expertise/", views.ManyExpertiseCreateView.as_view(), name="expertise-many-create"),
